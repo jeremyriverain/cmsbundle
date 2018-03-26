4d60b396be15b5b629ffc646e269b334350fcbac
jeremyriverain <jeremyriverain@netcourrier.com>
2018-03-26 09:15:12 +0200

    changement icon

--- src/Form/ResourceBaseType.php
+++ src/Form/ResourceBaseType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Geekco\CmsBundle\Entity\Resource;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ResourceBaseType extends AbstractType
 {
